ed073008bb496e9ebf58b311affc4bd1bac03a29
Nicolas <github@liliwol.fr>
2022-03-01 15:08:47 +0100

    Démo 2 ajout

---
+++ Module 3 - Développer des librairies en Javascript/Démo 2 - Créer une classe/Parcours objet et affichage HTML/CreerEtManipulerClasse3.js
@@ -0,0 +1,89 @@
+// Objet dynamique
+let eleves = [{
+    "id": 1,
+    "firstname": "Tammie",
+    "lastname": "Growcott",
+    "age": 31,
+    "image": "https://robohash.org/utametet.bmp?size=50x50&set=set1",
+    "biography": 5
+}, {
+    "id": 2,
+    "firstname": "Gifford",
+    "lastname": "Sturch",
+    "age": 94,
+    "image": "https://robohash.org/quisofficiaaut.png?size=50x50&set=set1",
+    "biography": 1
+}, {
+    "id": 3,
+    "firstname": "Meir",
+    "lastname": "Hullah",
+    "age": 100,
+    "image": "https://robohash.org/minimaametex.jpg?size=50x50&set=set1",
+    "biography": 3
+}, {
+    "id": 4,
+    "firstname": "Leonore",
+    "lastname": "Whitworth",
+    "age": 54,
+    "image": "https://robohash.org/veritatisvelitaque.jpg?size=50x50&set=set1",
+    "biography": 2
+}, {
+    "id": 5,
+    "firstname": "Rafael",
+    "lastname": "Artrick",
+    "age": 66,
+    "image": "https://robohash.org/utassumendasint.bmp?size=50x50&set=set1",
+    "biography": 1
+}, {
+    "id": 6,
+    "firstname": "Kittie",
+    "lastname": "Blumer",
+    "age": 52,
+    "image": "https://robohash.org/repellatnecessitatibusqui.jpg?size=50x50&set=set1",
+    "biography": 1
+}, {
+    "id": 7,
+    "firstname": "Dexter",
+    "lastname": "Gaunt",
+    "age": 20,
+    "image": "https://robohash.org/quoddistinctioet.bmp?size=50x50&set=set1",
+    "biography": 1
+}, {
+    "id": 8,
+    "firstname": "Moritz",
+    "lastname": "Spirit",
+    "age": 64,
+    "image": "https://robohash.org/eossuntvoluptatum.png?size=50x50&set=set1",
+    "biography": 1
+}, {
+    "id": 9,
+    "firstname": "Gibby",
+    "lastname": "Barfoot",
+    "age": 18,
+    "image": "https://robohash.org/voluptatemnihildistinctio.bmp?size=50x50&set=set1",
+    "biography": 2
+}, {
+    "id": 10,
+    "firstname": "Denyse",
+    "lastname": "Aldrin",
+    "age": 3,
+    "image": "https://robohash.org/dictarerumlaudantium.bmp?size=50x50&set=set1",
+    "biography": 1
+}];
+
+// Parcours du tableau et Affichage dans le dom
+
+// 1. Parcours du tableau
+// Choisissez votre boucle
+// eleves.forEach( eleve => console.log(eleve));
+// for (let eleve of eleves)
+
+// 2. A chaque itération ajouter un li dans le ul
+// 2.1 Sélection de l'élément ul
+// document.getElementById
+
+// 2.2 Création de l'élément li
+// document.createElement
+
+// 2.3 Affectation de la valeur à li
+// 2.4 Ajout de li dans ul
