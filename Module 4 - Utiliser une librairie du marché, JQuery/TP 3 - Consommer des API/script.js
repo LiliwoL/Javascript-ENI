96d818c355865ce6bed69a35c6b1684a8de9d709
Nicolas <github@liliwol.fr>
2022-03-04 10:15:56 +0100

    Démo 2 ajout

--- Module 4 - Utiliser une librairie du marché, JQuery/TP 3 - Consommer des API/script.js
+++ Module 4 - Utiliser une librairie du marché, JQuery/TP 3 - Consommer des API/script.js
@@ -5,15 +5,33 @@ jQuery(document).ready(
             "click",
             fonctionFetch
         );
-
-        // Affichage de la carte
-        var map = L.map('map').setView([47.468700, -0.558810], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
     }
 );
+
+/*
+    L'affichage d'une carte nécessite:
+    1. d'avoir chargé la librairie leaflet ET son CSS
+    2. d'avoir ajouté un élément container dans le HTML
+        <div id="map"></div>
+    3. d'avoir spécifié une hauteur par défaut à cet élément container (sinon, vous ne le verrez pas s'afficher!)
+        <style>
+            #map { height: 500px; }
+        </style>
+    4. Charger la carte dans l'élément container
+        var map = L.map('map').setView([47.468700, -0.558810], 13);
+    5. Ajouter à cette carte une couche de tuile ( tileLayer)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; Formation ENI Javascript'
+        }).addTo(map);
+
+ */
+// Affichage de la carte
+var map = L.map('map').setView([47.468700, -0.558810], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; Formation ENI Javascript'
+}).addTo(map);
+
+
 
 // Fonction appelée uniquement au clic sur le bouton #boutonFetch
 function fonctionFetch(){
@@ -41,7 +59,7 @@ function fonctionFetch(){
 
 // Fonction de rappel utilisée en cas de fetch réussi
 function traiterDonnesRecues(data){
-    console.log(typeof data);
+    //console.log(typeof data);
 
     // Afficher dans le DOM
     //console.log(data);
@@ -58,8 +76,6 @@ function traiterDonnesRecues(data){
 }
 
 function ajoutDansLeDom(data){
-
-    console.log(data);
     // On ne veut que les données du tabelau fields
     data = data.fields;
 
@@ -84,4 +100,11 @@ function ajoutDansLeDom(data){
 
     // Sélection du tableau dans #listeResultats
     $('#listeResultats table').append(tr);
+
+
+    // Ajout du marker dans la table
+    // https://leafletjs.com/SlavaUkraini/examples/custom-icons/
+    L.marker([data.geo_point_2d[0], data.geo_point_2d[1]])
+        .addTo(map)
+        .bindPopup("<b>" + data.tico + "</b><br/>" + data.adrs);
 }
